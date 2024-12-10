984ccc0003aef75626fe5488967e2dbc31e5ff4a
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-12-10 21:39:13 +0500

    Task4

--- Tyuiu.SanzyapovOD.Sprint6.Task4.V1.Lib/DataService.cs
+++ Tyuiu.SanzyapovOD.Sprint6.Task4.V1.Lib/DataService.cs
@@ -1,8 +1,22 @@
-﻿using Tyuiu.SanzyapovOD.Sprint6.Task4.V1.Lib; 
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.SanzyapovOD.Sprint6.Task4.V1.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task4V1
     {
-
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(Math.Sin(x) / (x + 1.2) + Math.Cos(x) * 7 * x - 2, 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+        }
     }
 }
--- Tyuiu.SanzyapovOD.Sprint6.Task4.V1.Test/DataServiceTest.cs
+++ Tyuiu.SanzyapovOD.Sprint6.Task4.V1.Test/DataServiceTest.cs
@@ -7,6 +7,23 @@ namespace Tyuiu.SanzyapovOD.Sprint6.Task4.V1.Test
         [TestMethod]
         public void TestMethod1()
         {
+            int start = -5;
+            int stop = 5;
+            DataService ds = new DataService();
+            double[] wait = new double[11];
+            wait[0] = -12.18;
+            wait[1] = 16.03;
+            wait[2] = 18.87;
+            wait[3] = 4.96;
+            wait[4] = -9.99;
+            wait[5] = -2.00;
+            wait[6] = 2.16;
+            wait[7] = -7.54;
+            wait[8] = -22.76;
+            wait[9] = -20.45;
+            wait[10] = 7.77;
+            double[] res = ds.GetMassFunction(start, stop);
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
