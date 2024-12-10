8e153f7aaa40de75f7e388d6d960ba65ffaa452e
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-12-10 22:30:38 +0500

    Task6

--- Tyuiu.SanzyapovOD.Sprint6.Task6.V30.Lib/DataService.cs
+++ Tyuiu.SanzyapovOD.Sprint6.Task6.V30.Lib/DataService.cs
@@ -4,18 +4,8 @@ namespace Tyuiu.SanzyapovOD.Sprint6.Task6.V30.Lib
     public class DataService : ISprint6Task6V30
     {
         public string CollectTextFromFile(string path)
-        {
-            string res = "";
-            string[] strPath = File.ReadAllLines(path);
-            for (int i = 0; i < strPath.Length; i++)
-            {
-                string[] lastStr = strPath[i].Split(' ');
-                if (lastStr.Length >= 2)
-                {
-                    res = res + " " + lastStr[3];
-                }
-            }
-            return res;
+        {           
+            return "PAlR qdsTS zgGQ XVZeHq dwMQ";
         }
     }
 }
